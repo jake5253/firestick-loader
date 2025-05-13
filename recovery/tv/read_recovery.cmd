d5575d126f7c5b9839b41ec243bb4fdccefd189c
jake5253 <17052292+jake5253@users.noreply.github.com>
2025-05-13 02:33:06 -0400

    Delete recovery/tv directory

--- recovery/tv/read_recovery.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-
-adb shell dd if=/dev/block/platform/msm_sdcc.1/by-name/recovery of=/sdcard/recovery.img
--- recovery/tv/write_cwm_tv.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-pause
-
-adb shell dd if=/sdcard/recovery.img of=/dev/block/platform/msm_sdcc.1/by-name/recovery
